f7152d8e79692db485655555a9f7cb70821f7f12
Christoph Reimers <christoph@superservice-international.com>
2015-04-13 19:24:39 +0200

    finally, them 1.7 migrations

--- cmsplugin_iconlist/migrations_django/0001_initial.py
+++ cmsplugin_iconlist/migrations_django/0001_initial.py
@@ -14,10 +14,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Icon',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
-                ('icon', models.CharField(choices=[('fa-facebook', 'facebook'), ('fa-foursquare', 'foursquare'), ('fa-google-plus', 'google-plus'), ('fa-instagram', 'instagram'), ('fa-linkedin', 'linkedin'), ('fa-twitter', 'twitter'), ('fa-xing', 'xing'), ('fa-yelp', 'yelp'), ('fa-youtube-play', 'youtube')], verbose_name='icon color', max_length=200)),
-                ('color', models.CharField(default='#000000', verbose_name='icon color', max_length=200)),
-                ('background_color', models.CharField(default='#ffffff', verbose_name='background color', max_length=200)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('icon', models.CharField(choices=[('fa-facebook', 'facebook'), ('fa-foursquare', 'foursquare')], verbose_name='icon', max_length=200)),
+                ('color', models.CharField(verbose_name='icon color', max_length=200, default='#000000')),
+                ('background_color', models.CharField(verbose_name='background color', max_length=200, default='#ffffff')),
                 ('link', models.URLField(verbose_name='link')),
             ],
             options={
@@ -27,8 +27,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='IconList',
             fields=[
-                ('cmsplugin_ptr', models.OneToOneField(serialize=False, to='cms.CMSPlugin', parent_link=True, auto_created=True, primary_key=True)),
-                ('name', models.CharField(default='', verbose_name='name', max_length=32)),
+                ('cmsplugin_ptr', models.OneToOneField(primary_key=True, serialize=False, parent_link=True, to='cms.CMSPlugin', auto_created=True)),
+                ('name', models.CharField(verbose_name='name', max_length=32, default='')),
             ],
             options={
                 'abstract': False,
