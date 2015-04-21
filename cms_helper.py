71f369eba56bcef68adc09696c12ae05cee0b4cb
Christoph Reimers <christoph@superservice-international.com>
2015-04-21 09:46:31 +0200

    initialized cms_helper.py

---
+++ cms_helper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+gettext = lambda s: s
+
+HELPER_SETTINGS = {
+    'NOSE_ARGS': [
+        '-s',
+    ],
+    'INSTALLED_APPS': [
+    ],
+    'LANGUAGE_CODE': 'de',
+    'LANGUAGES': (
+        ('de', gettext('de')),
+    ),
+    'CMS_LANGUAGES' = {
+        ## Customize this
+        1: [
+            {
+                'redirect_on_fallback': True,
+                'hide_untranslated': False,
+                'name': gettext('de'),
+                'code': 'de',
+                'public': True,
+            },
+        ],
+        'default': {
+            'redirect_on_fallback': True,
+            'public': True,
+            'hide_untranslated': False,
+        },
+    },
+    'MIDDLEWARE_CLASSES': [
+        'django.contrib.messages.middleware.MessageMiddleware',
+    ],
+    'MIGRATION_MODULES': {
+    },
+}
