e16e205a7a2d000f519aac706dcde3b297078299
creimers <christoph@superservice-international.com>
2016-04-11 21:32:01 +0200

    update python version in setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(
         'Programming Language :: Python',
         'Framework :: Django',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
